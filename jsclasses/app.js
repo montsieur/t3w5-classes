a40f2e8ca11c1c6b5617293eeec59c249c816ed1
montsieur <dndott@gmail.com>
2024-11-16 10:55:14 +1100

    feat: error handling, static and private properties

--- jsclasses/app.js
+++ jsclasses/app.js
@@ -1,11 +1,55 @@
+// create class
 class Dog {
+    // constructor
     constructor(name, breed, age, color) {
-        this.name = name;
+        this.name = name; // instance property
         this.breed = breed;
         this.age = age;
         this.color = color;
+        Dog.#numberofDogs++;
+    }
+    
+    // static property
+    static #numberofDogs = 0; 
+
+    // static method
+    static resetDogCount(){
+        Dog.#numberofDogs = 0;
+    }
+
+    // static set numberofDogs(newValue){
+    //     return Dog.#numberofDogs++;
+    // }
+
+    // instance method
+    bark() {
+        // console.log(`Woof! Woof! I am ${this.name}.`);
+        throw new BarkError(this.name, this.breed);
     }
 }
 
 let brockie = new Dog("Brockie", "Golden Retriever", '5', "Golden");
-console.log(brockie);
+// console.log(brockie.bark());
+
+// create unique Error class by inheriting from Error
+class BarkError extends Error { 
+    constructor(dogName, dogBreed) {
+        // super() is called to use the Error Class properties/methods
+        super(`${dogName} of breed ${dogBreed} failed to bark`);
+        // override default Error class name
+        this.name = "BarkError";
+    }
+}
+
+// let newError = new Error()
+try {
+    brockie.bark()
+} catch (error) {
+    // throw new BarkError(brockie.name, brockie.breed);
+    if (BarkError){
+        console.log("Bark error occurred");
+    }
+}
+
+Dog.numberofDogs = 10;
+console.log(Dog.numberofDogs);
