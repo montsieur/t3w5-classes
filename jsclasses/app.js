ce0122a32457f5a914ff8ba331a15c7ec46632a9
montsieur <dndott@gmail.com>
2024-11-16 10:02:45 +1100

    feat: example dog class

---
+++ jsclasses/app.js
@@ -0,0 +1,11 @@
+class Dog {
+    constructor(name, breed, age, color) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+}
+
+let brockie = new Dog("Brockie", "Golden Retriever", '5', "Golden");
+console.log(brockie);
